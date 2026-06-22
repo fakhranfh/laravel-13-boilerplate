7d417dccda7e3ee70b623d3b9ec59083c7ed9a49
Muhammad Fakhran Hadyan <fakhranhadyan@gmail.com>
2026-06-22 14:08:54 +0700

    feat: delete reset password on route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\EnsureTokenIsValid;
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
@@ -9,15 +8,6 @@ use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('/reset-password', function (Request $request) {
-    
-        return view('auth.reset-password', [
-            'token' => $request->query('token'),
-            'email' => $request->query('email'),
-        ]);
-        
-    })->middleware([EnsureTokenIsValid::class])->name('password.reset');
-
     Route::get('', function () {
         return view('auth.login');
     })->name('login');
@@ -43,23 +33,27 @@ Route::post('/logout', function (Request $request) {
 Route::get('/preview-reset-password', function () {
     $user = User::first();
 
-    if (!$user) return "No user found. Please create a user first.";
+    if (! $user) {
+        return 'No user found. Please create a user first.';
+    }
 
     $token = Password::getRepository()->create($user);
-    
+
     $notification = new ResetPassword($token);
-    
+
     return $notification->toMail($user)->render();
 });
 
 Route::get('/preview-verify-email', function () {
     $user = User::first();
 
-    if (!$user) return "No user found. Please create a user first.";
+    if (! $user) {
+        return 'No user found. Please create a user first.';
+    }
 
     $token = Password::getRepository()->create($user);
-    
+
     $notification = new VerifyEmail($token);
-    
+
     return $notification->toMail($user)->render();
 });
