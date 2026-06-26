9ff925c20a7ca10583f85485d7041278c72aaf58
Muhammad Fakhran Hadyan <fakhranhadyan@gmail.com>
2026-06-26 11:28:58 +0700

    fix(route): delete preview email

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Auth\Notifications\ResetPassword;
-use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -31,31 +27,3 @@ Route::post('/logout', function (Request $request) {
 
     return redirect()->route('login');
 })->middleware('auth')->name('logout');
-
-Route::get('/preview-reset-password', function () {
-    $user = User::first();
-
-    if (! $user) {
-        return 'No user found. Please create a user first.';
-    }
-
-    $token = Password::getRepository()->create($user);
-
-    $notification = new ResetPassword($token);
-
-    return $notification->toMail($user)->render();
-});
-
-Route::get('/preview-verify-email', function () {
-    $user = User::first();
-
-    if (! $user) {
-        return 'No user found. Please create a user first.';
-    }
-
-    $token = Password::getRepository()->create($user);
-
-    $notification = new VerifyEmail($token);
-
-    return $notification->toMail($user)->render();
-});
