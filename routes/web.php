97589403bc5ec12e4d72f6d464720e291a7a84d4
Muhammad Fakhran Hadyan <fakhranhadyan@gmail.com>
2026-06-26 16:06:03 +0700

    feat: delete product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,8 +19,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
 
     Route::get('product/data/list', [ProductController::class, 'list'])->name('product.list');
-    Route::resource('product', ProductController::class);
-});
+    });
 
 Route::post('/logout', function (Request $request) {
     auth()->logout();
